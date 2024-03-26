3e88f76718386cc36e0b2f3cb17708d8cd99a480
rishtansoft <rishtansoft@gmail.com>
2024-03-26 16:46:15 +0500

    table style changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import './App.css'
-import { Routes, Route } from "react-router-dom"
-import Teacher from './pages/teacher'
-import Group from './pages/group'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Teacher from './pages/teacher';
+import Group from './pages/group';
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Teacher></Teacher>}></Route>
-    </Routes>
-    <Routes>
-      <Route path='/group' element={<Group></Group>}></Route>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Teacher></Teacher>}></Route>
+      </Routes>
+      <Routes>
+        <Route path='/group' element={<Group></Group>}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/teacher/index.jsx
+++ src/pages/teacher/index.jsx
@@ -1,30 +1,27 @@
 import styles from "./index.module.css"
-import { CiSearch } from "react-icons/ci";
 
 function Teacher() {
   return (
     <div className={styles.home}>
-      <h1>O'qituvchilar</h1>
-      <div className={styles.search}>
-        <h3><CiSearch /></h3>
-        <input type="text" placeholder="Qiqiruv" />
-      </div>
-      <div className={styles.table}>
-        <table>
-          <thead>
+      <h1>O&apos;qituvchilar</h1>
+      
+      <div className={styles.tableWrapper}>
+        <table className={styles.table}>
+          <thead className={styles.thead}>
             <tr>
               <th>ID</th>
               <th>Ism Familfa</th>
               <th>Guruh</th>
             </tr>
           </thead>
+
           <tbody>
             <tr>
               <td>23456</td>
               <td>Durdona Egamberdiyeva</td>
               <td>Beginner 1</td>
             </tr>
-          </tbody>
+
             <tr>
               <td>23456</td>
               <td>Durdona Egamberdiyeva</td>
@@ -41,6 +38,8 @@ function Teacher() {
               <td>Durdona Egamberdiyeva</td>
               <td>Beginner 1</td>
             </tr>
+          </tbody>
+            
         </table>
       </div>
     </div>
