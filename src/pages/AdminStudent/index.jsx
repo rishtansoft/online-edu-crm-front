5725fd2a0765045bd23c1f62d07aa645ba68eda9
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2024-04-01 20:22:25 +0500

    admin student

---
+++ src/pages/AdminStudent/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./index.module.css"
+
+function AdminStudent() {
+  return (
+    <div className={styles.admin_student}>
+        AdminStudent
+    </div>
+  )
+}
+
+export default AdminStudent
